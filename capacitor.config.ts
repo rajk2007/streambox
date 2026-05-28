fe1721da1a34ea77e9874e885217ff769997da91
rajk2007 <rajkarmakar9564@gmail.com>
2026-05-28 11:37:50 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.rajk2007.streambox',
   appName: 'StreamBox',
-  webDir: 'dist'
+  webDir: 'dist/client'
 };
 
 export default config;
